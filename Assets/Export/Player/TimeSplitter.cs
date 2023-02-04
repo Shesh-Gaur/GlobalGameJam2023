6d59bf00021f22cf276d0a41bfcd1a6b5a5656ca
Shesh Gaur <sheshg3@gmail.com>
2023-02-04 16:55:50 -0500

    Imported Haider's Stuff

---
+++ Assets/Export/Player/TimeSplitter.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+public class TimeSplitter : MonoBehaviour
+{
+    [SerializeField] PlayerController controller;
+
+    [SerializeField] bool inAltWorld = false;
+
+    [SerializeField] Vector3 Offset;
+
+    PlayerControls controls;
+    PlayerControls.OnFootActions onFoot;
+
+    private void Teleport()
+    {
+        Debug.Log("Teleporting");
+        if(inAltWorld)
+        {
+            transform.position = transform.position - Offset;
+            if (controller.isHoldingObject)
+            {
+                controller._heldObject.transform.position = controller._heldObject.transform.position - Offset;
+            }
+            inAltWorld = false;
+        }
+        else
+        {
+            transform.position = transform.position + Offset;
+            if (controller.isHoldingObject)
+            {
+                controller._heldObject.transform.position = controller._heldObject.transform.position + Offset;
+            }
+            inAltWorld = true;
+        }
+    }
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        onFoot = controls.onFoot;
+
+        onFoot.Swap.performed += _ => Teleport();
+
+        onFoot.Enable();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void Reset()
+    {
+        Offset = new Vector3(0, 1000, 0);
+    }
+}
--- Assets/Scripts/Enemies/WalkingEnemy.cs
+++ Assets/Scripts/Enemies/WalkingEnemy.cs
@@ -9,7 +9,9 @@ public class WalkingEnemy : Enemy
     // Start is called before the first frame update
     void Start()
     {
-        
+        detection = detectionMax;
+        statusLight = GetComponentInChildren<Light>();
+        statusLight.spotAngle = sightAngle;
     }
 
     // Update is called once per frame
@@ -18,13 +20,34 @@ public class WalkingEnemy : Enemy
         Listen();
         See();
         SelectPatrolPoint();
-        Steer();
-        Avoidance();
+
+        if (distrationTimer <= 0)
+        {
+            Steer();
+            Avoidance();
+        }
+        else
+        {
+            distrationTimer -= Time.deltaTime;
+            Distracted();
+        }
+
     }
 
     void Steer()
     {
+        Vector3 newVel = GetComponent<Rigidbody>().velocity;
+        Vector3 desiredVel = target.transform.position - transform.position;
 
+        Vector3 steering = desiredVel - GetComponent<Rigidbody>().velocity;
+        steering = steering.normalized;
+
+        newVel += steering * movementSpeed * Time.deltaTime;
+        newVel.y = GetComponent<Rigidbody>().velocity.y;
+
+        GetComponent<Rigidbody>().velocity = newVel;
+        newVel.y = 0;
+        transform.rotation = Quaternion.LookRotation(newVel);
     }
 
     void Avoidance()
@@ -37,4 +60,11 @@ public class WalkingEnemy : Enemy
 
     }
 
+    void Distracted()
+    {
+        Vector3 dir = lastHeardSoundPos - transform.position;
+        dir.y = 0.0f;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), movementSpeed * Time.deltaTime);
+        statusLight.color = Color.Lerp(statusLight.color, Color.yellow, Time.deltaTime);
+    }
 }
