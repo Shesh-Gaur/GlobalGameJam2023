39385aaf7c9bfebfe8af04cb75cace92efde6360
Haider <Haiderali360@gmail.com>
2023-02-05 13:53:01 -0500

    Placed Enemies, Alt World exists.

--- Assets/Scripts/Player/TimeSplitter.cs
+++ Assets/Scripts/Player/TimeSplitter.cs
@@ -8,12 +8,16 @@ public class TimeSplitter : MonoBehaviour
 {
     [SerializeField] PlayerController controller;
 
+    [SerializeField] GameObject _camera;
+
     [SerializeField] bool inAltWorld = false;
 
     [SerializeField] Vector3 Offset;
 
     PlayerControls controls;
     PlayerControls.OnFootActions onFoot;
+
+    bool cameraSwapped;
 
     private void Teleport()
     {
@@ -35,6 +39,32 @@ public class TimeSplitter : MonoBehaviour
                 controller._heldObject.transform.position = controller._heldObject.transform.position + Offset;
             }
             inAltWorld = true;
+        }
+    }
+
+    void View()
+    {
+        if (inAltWorld)
+        {
+            if (!cameraSwapped)
+            {
+                _camera.transform.position = _camera.transform.position - Offset;
+            }
+            else
+            {
+                _camera.transform.position = _camera.transform.position + Offset;
+            }
+        }
+        else
+        {
+            if (!cameraSwapped)
+            {
+                _camera.transform.position = _camera.transform.position + Offset;
+            }
+            else
+            {
+                _camera.transform.position = _camera.transform.position - Offset;
+            }
         }
     }
 
@@ -61,5 +91,19 @@ public class TimeSplitter : MonoBehaviour
     void Reset()
     {
         Offset = new Vector3(0, 1000, 0);
+    }
+
+    void Update()
+    {
+        if(onFoot.View.ReadValue<float>() > 0 && !cameraSwapped)
+        {
+            View();
+            cameraSwapped = true;
+        }
+        if(cameraSwapped && onFoot.View.ReadValue<float>() == 0)
+        {
+            View();
+            cameraSwapped = false;
+        }
     }
 }
